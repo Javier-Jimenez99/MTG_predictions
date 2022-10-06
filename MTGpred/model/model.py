c944e58220b1a48951966d35bb068094a39fed68
Javier-Jimenez99 <javijj99@gmail.com>
2022-10-06 15:15:28 +0200

    fix model data

--- MTGpred/model/model.py
+++ MTGpred/model/model.py
@@ -12,24 +12,24 @@ class DeckEncoder(nn.Module):
     def __init__(self):
         super(DeckEncoder,self).__init__()
         # Decks are 2D tensors of shape (1,100,768)
-        self.conv1 = nn.Conv1d(1,16,3,padding="same")   # (16,100,768)
-        self.maxpool1 = nn.MaxPool1d(2)                 # (16,50,384)
-        self.conv2 = nn.Conv1d(16,32,3,padding="same")  # (32,50,384)
-        self.maxpool2 = nn.MaxPool1d(2)                 # (32,25,192)
-        self.conv3 = nn.Conv1d(32,64,3,padding="same")  # (64,25,192)
-        self.maxpool3 = nn.MaxPool1d(2)                 # (64,12,96)
-        self.conv4 = nn.Conv1d(64,128,3,padding="same") # (128,12,96)
-        self.maxpool4 = nn.MaxPool1d(2)                 # (128,6,48)
-        self.conv5 = nn.Conv1d(128,256,3,padding="same")# (256,6,48)
-        self.maxpool5 = nn.MaxPool1d(2)                 # (256,3,24)
-        self.conv6 = nn.Conv1d(256,512,3,padding="same")# (512,3,24)
-        self.maxpool6 = nn.MaxPool1d(3)                 # (512,1,8)
-        self.flatten = nn.Flatten()                     # (4096)
-        self.fc1 = nn.Linear(4096,512)                  # (512)
-        self.fc2 = nn.Linear(512,256)                   # (256)
+        self.conv1 = nn.Conv2d(1,16,3,padding="same")       # (16,100,768)
+        self.maxpool1 = nn.MaxPool2d(2)                     # (16,50,384)
+        self.conv2 = nn.Conv2d(16,32,3,padding="same")      # (32,50,384)
+        self.maxpool2 = nn.MaxPool2d(2)                     # (32,25,192)
+        self.conv3 = nn.Conv2d(32,64,3,padding="same")      # (64,25,192)
+        self.maxpool3 = nn.MaxPool2d(2)                     # (64,12,96)
+        self.conv4 = nn.Conv2d(64,128,3,padding="same")     # (128,12,96)
+        self.maxpool4 = nn.MaxPool2d(2)                     # (128,6,48)
+        self.conv5 = nn.Conv2d(128,256,3,padding="same")    # (256,6,48)
+        self.maxpool5 = nn.MaxPool2d(2)                     # (256,3,24)
+        self.conv6 = nn.Conv2d(256,512,3,padding="same")    # (512,3,24)
+        self.maxpool6 = nn.MaxPool2d(3)                     # (512,1,8)
+        self.flatten = nn.Flatten()                         # (4096)
+        self.fc1 = nn.Linear(4096,512)                      # (512)
+        self.fc2 = nn.Linear(512,256)                       # (256)
 
     def forward(self,deck):
-        deck = deck.unsqueeze(0)
+        deck = deck.unsqueeze(1)
 
         # deck is a 2D tensor of shape (1,100,768)
         deck = self.conv1(deck)
@@ -76,7 +76,7 @@ class WinnerPredictor(nn.Module):
         deck1 = self.encoder(deck1)
         deck2 = self.encoder(deck2)
         output = self.final_classifier(torch.cat((deck1,deck2),dim=1))
-        winner = F.sigmoid(output)
+        winner = torch.sigmoid(output).squeeze()
         return winner
 
 def accuracy(y_pred,y_true):
@@ -107,10 +107,9 @@ def train(split_ratio=0.8,batch_size=16,epochs=10,lr=0.001,cards_path = "data\At
     for epoch in range(epochs):
         print(f"======= EPOCH {epoch+1}/{epochs} =======")
         train_losses = []
-        for batch in tqdm(train_dataloader,desc="TRAIN"):
-            print(batch)
-            input = batch[0].to(device)
-            target = batch[1].to(device)
+        for input,target in tqdm(train_dataloader,desc="TRAIN"):
+            input = input.to(device)
+            target = target.to(device).to(torch.float32)
             optimizer.zero_grad()
             winner = model(input[:,0],input[:,1])
             loss = criterion(winner,target)
@@ -122,11 +121,13 @@ def train(split_ratio=0.8,batch_size=16,epochs=10,lr=0.001,cards_path = "data\At
         with torch.no_grad():
             test_losses = []
             accuracies = []
-            for input,output in tqdm(test_dataloader,desc="TEST"):
+            for input,target in tqdm(test_dataloader,desc="TEST"):
+                input = input.to(device)
+                target = target.to(device).to(torch.float32)
                 winner = model(input[:,0],input[:,1])
-                loss = criterion(winner,output)
+                loss = criterion(winner,target)
                 test_losses.append(loss.item())
-                accuracies.append(accuracy(winner,output > 0.5))
+                accuracies.append(accuracy(winner,target > 0.5))
             print(f"Test loss: {sum(test_losses)/len(test_losses)}")
             print(f"Test accuracy: {sum(accuracies)/len(accuracies)}")
 
