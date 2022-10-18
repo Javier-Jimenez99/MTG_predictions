2f87891b4469bd819221bd2829b960887df45af9
Javier-Jimenez99 <javijj99@gmail.com>
2022-10-18 19:49:57 +0200

    update setup

--- setup.py
+++ setup.py
@@ -5,8 +5,19 @@ setuptools.setup(
     use_scm_version=True,
     author="Javier Jimenez",
     packages=setuptools.find_packages(),
-    python_requires=">=3.9",
+    python_requires=">=3.8",
     setup_requires=["setuptools_scm"],
     install_requires=[
+        "torch",
+        "numpy",
+        "pandas",
+        "transformers",
+        "deepspeed",
+        "pymongo",
+        "wandb",
+        "typer",
+        "sentencepiece",
+        "black",
+        #"mpi4py", Install with conda
     ],
 )
